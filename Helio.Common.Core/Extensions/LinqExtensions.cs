65675665306a2f8dbbb020e43496a873200b7761
HelioVernet <34055737+HelioVernet@users.noreply.github.com>
2020-08-04 18:41:11 +0200

    First push

---
+++ Helio.Common.Core/Extensions/LinqExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Helio.Common.Core.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource,bool>> predicate)
+        {
+            if (condition)
+            {
+                return source.Where(predicate);
+            }
+            else
+            {
+                return source;
+            }
+        }
+    }
+}
---
+++ Helio.Common.Core/Logging/Logs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helio.Common.Core.Logging
+{
+    public class Logs
+    {
+        public void MessageLog(string message)
+        {
+
+        }
+    }
+}
